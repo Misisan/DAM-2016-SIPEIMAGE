4e7b4856f46e4ef9629a036b8160b072a00a9b9c
Miguel A Simeon <miguelsimeon@gmail.com>
2015-11-25 12:15:43 +0100

    Lección sobre variables

---
+++ L05Variable/src/Variable.java
@@ -0,0 +1,37 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		// Variables
+		
+		// tipo nombre_variable = valor;
+		int numeroEntero=4;
+		int numeroSinDefinir=0;
+		int numeroNegativo=-56;
+		
+		// Números decimales
+		float decimal=12.5f;
+		double decimalMasLargo=12.5;
+		
+		// tipo char
+		char letra='a';
+		String letraString="Paco";
+		
+		// tipo boolean
+		boolean semaforo=true;
+		
+		//Imprimir números
+		System.out.println(numeroEntero);
+		System.out.println(numeroSinDefinir);
+		
+		System.out.println(decimal);
+		System.out.println(decimalMasLargo);
+		
+		System.out.println(letra);
+		System.out.println(letraString);
+		
+		System.out.println(semaforo);
+	}
+
+}
